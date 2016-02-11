62baa5043e09624e45c45fc5392bd2d3ca0a4a86
jdmor <jona.morrissey@gmail.com>
2016-02-11 00:58:45 +0000

    Load lighthouse images on button click

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,12 +1,28 @@
+'use strict';
+
 $(function() {
 
-  $.getJSON('http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?', {
-    tags: 'lighthouse',
-    tagmode: 'any',
-    format: 'json'
-  })
-    .done(function(data) {
-      console.log(data);
-    });
+  var handlers = {
+    loadImages: function() {
+      $.getJSON('http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?', {
+        tags: 'lighthouse',
+        tagmode: 'any',
+        format: 'json'
+      })
+        .done(function(data) {
+          $('.container').empty();
+          var images = data.items;
+          images.forEach(function(image) {
+            console.log(image.media.m);
+            $('<img>').attr('src', image.media.m).appendTo('.container');
+          });
+        });
+    }
+  }
+
+
+  $('#loadImages').on('click', function() {
+    handlers.loadImages();
+  });
 
 });
